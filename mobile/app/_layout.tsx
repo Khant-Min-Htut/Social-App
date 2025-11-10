fc895b945b5386cc9d2069421fd7b84d8e615421
khantMinHtut <khantminhtut009@gmail.com>
2025-11-10 23:25:21 +0700

    login page ui

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,19 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+
+import { Slot } from "expo-router";
 // import { StackScreen } from "react-native-screens";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
 }
 
 // <Stack.Screen name="index" options={{ title: "Home" }} />
